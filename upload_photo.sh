f89bdc809ec15a8d4f5295838b0a897ef43f1763
Oliver Parson <oliparson@gmail.com>
2020-05-03 22:06:06 +0100

    loop indefinitely

--- upload_photo.sh
+++ upload_photo.sh
@@ -1,9 +1,18 @@
-DATE=$(date +"%Y-%m-%d_%H%M%S")
-FILENAME="/home/pi/Pictures/$DATE.jpg"
 
-raspistill -o $FILENAME
+while :
+do
+	DATE=$(date +"%Y-%m-%d_%H%M%S")
+	FILENAME="/home/pi/Pictures/$DATE.jpg"
 
-curl -X POST --data-binary @$FILENAME \
--H "Authorization: Bearer $OAUTH2_TOKEN" \
--H "Content-Type: image/jpg" \
-"https://storage.googleapis.com/upload/storage/v1/b/catflap-photos-raw/o?uploadType=media&name=$DATE.jpy"
+	# Take photo and save to Raspberry Pi SD card
+	raspistill -o $FILENAME
+
+	# Upload photo to Google Cloud Storage bucket
+	curl -X POST --data-binary @$FILENAME \
+	-H "Authorization: Bearer $OAUTH2_TOKEN" \
+	-H "Content-Type: image/jpg" \
+	"https://storage.googleapis.com/upload/storage/v1/b/catflap-photos-raw/o?uploadType=media&name=$DATE.jpy"
+
+	# Remove photo from Raspberry Pi SD card
+	rm $FILENAME
+done
