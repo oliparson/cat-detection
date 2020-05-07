d6c099eb10b59a7955a2e42a10d1bbbce10ef67d
Oliver Parson <oliparson@gmail.com>
2020-05-07 22:02:12 +0100

    reduce resolution and fix typo in gcs filename

--- upload_photo.sh
+++ upload_photo.sh
@@ -5,13 +5,13 @@ do
 	FILENAME="/home/pi/Pictures/$DATE.jpg"
 
 	# Take photo and save to Raspberry Pi SD card
-	raspistill -rot 180 -o $FILENAME
+	raspistill -rot 180 -w 800 -h 600 -o $FILENAME
 
 	# Upload photo to Google Cloud Storage bucket
 	curl -X POST --data-binary @$FILENAME \
 	-H "Authorization: Bearer $OAUTH2_TOKEN" \
 	-H "Content-Type: image/jpg" \
-	"https://storage.googleapis.com/upload/storage/v1/b/catflap-photos-raw/o?uploadType=media&name=$DATE.jpy"
+	"https://storage.googleapis.com/upload/storage/v1/b/catflap-photos-raw/o?uploadType=media&name=$DATE.jpg"
 
 	# Remove photo from Raspberry Pi SD card
 	rm $FILENAME
