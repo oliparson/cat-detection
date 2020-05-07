1ff9d055a3c90bca11af33c2ac4e3f3da87a3424
Oliver Parson <oliparson@gmail.com>
2020-05-07 23:36:40 +0100

    renew auth token if expired

--- upload_photo.sh
+++ upload_photo.sh
@@ -8,11 +8,20 @@ do
 	raspistill -rot 180 -w 800 -h 600 -o $FILENAME
 
 	# Upload photo to Google Cloud Storage bucket
-	curl -X POST --data-binary @$FILENAME \
+	response=$(curl -X POST --data-binary @$FILENAME \
 	-H "Authorization: Bearer $OAUTH2_TOKEN" \
 	-H "Content-Type: image/jpg" \
-	"https://storage.googleapis.com/upload/storage/v1/b/catflap-photos-raw/o?uploadType=media&name=$DATE.jpg"
-
+	"https://storage.googleapis.com/upload/storage/v1/b/catflap-photos-raw/o?uploadType=media&name=$DATE.jpg")
+	
 	# Remove photo from Raspberry Pi SD card
 	rm $FILENAME
+	
+	# Recreate auth code if it's expired
+	error_code=$(echo $response | jq -r '.error.code')
+	if [ "$error_code" == "401" ]
+	then
+		echo Renewing gcloud oauth token
+		OAUTH2_TOKEN=$(gcloud auth print-access-token)
+	fi
+
 done
